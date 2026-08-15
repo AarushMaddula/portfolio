ebd197c5c53eb4898f6bb825ea4e2755425f44e2
Aarush Maddula <100654110+aarushmaddula@users.noreply.github.com>
2026-08-15 00:14:01 -0400

    Add school image to education card

--- src/app/components/EducationCard.tsx
+++ src/app/components/EducationCard.tsx
@@ -3,6 +3,7 @@ type EducationCardProps = {
   description: string;
   date: string;
   location: string;
+  image_url: string;
 };
 
 export default function EducationCard({
@@ -10,26 +11,27 @@ export default function EducationCard({
   date,
   description,
   location,
+  image_url
 }: EducationCardProps) {
   return (
-    <div className="flex items-start gap-4 bg-normal p-4 rounded-xl">
-      <div className="flex-1/6 md:flex-1/12 flex justify-center items-center p-2 relative aspect-square bg-radial from-light rounded-full">
-        <img src={"/icons/grad-cap.svg"} alt="" className="invert" />
+    <div className="flex flex-col md:flex-row bg-normal rounded-xl overflow-hidden">
+      <div className="md:w-1/3 h-60 md:h-auto">
+        <img src={image_url} alt="" className="h-full w-full object-cover "/>
       </div>
 
-      <div className="flex-5/6">
+      <div className="flex-1 py-8 px-6 flex flex-col">
         <h3 className="text-2xl text-neutral font-semibold mb-2">{title}</h3>
-        <p className="text-sm text-muted mb-2">{description}</p>
-        <div className="flex gap-4">
-          <div className="flex gap-1">
+        <div className="flex gap-4 text-md text-muted mb-4">
+          <div className="flex items-center gap-1.5">
             <img src="/icons/calendar.svg" alt="" className="invert h-4" />
-            <p className="text-sm text-muted">{date}</p>
+            <p>{date}</p>
           </div>
-          <div className="flex gap-1">
+          <div className="flex items-center gap-1.5">
             <img src="/icons/pin.svg" alt="" className="invert h-4" />
-            <p className="text-sm text-muted">{location}</p>
+            <p>{location}</p>
           </div>
         </div>
+        <p className="text-md text-muted mb-2">{description}</p>
       </div>
     </div>
   );

068d5a4f46e199ac78110e6237df2fbacd1fb178
Aarush Maddula <100654110+aarushmaddula@users.noreply.github.com>
2026-08-17 22:35:20 -0400

    Make footer year dynamic

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
       <footer className="flex justify-center items-center gap-2 w-full p-6 text-sm text-muted border-t border-t-accent/20">
         <p>Made with Despair & Next.js</p>
         <span>|</span>
-        <p>&copy;2025 Aarush Maddula</p>
+        <p>&copy;{new Date().getFullYear()} Aarush Maddula</p>
       </footer>
     </>
   )
